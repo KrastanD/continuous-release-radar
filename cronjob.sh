0ddb05212cfe9312a63a3f5b1352de87fef22d1e
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-11-09 14:38:56 -0600

    Fix variable in cronjob shell script

--- cronjob.sh
+++ cronjob.sh
@@ -3,4 +3,4 @@
 CRR_HOME="/YOUR_PATH/continuous-release-radar/"
 GO_PATH="/YOUR_PATH/go"
 
-(crontab -l ; echo "0 12 * * 2 cd $CRR_HOME && GO_PATH run . 2>&1") | crontab -
+(crontab -l ; echo "0 12 * * 2 cd $CRR_HOME && $GO_PATH run . 2>&1") | crontab -
